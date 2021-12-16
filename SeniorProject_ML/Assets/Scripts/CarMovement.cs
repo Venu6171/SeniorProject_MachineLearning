82bb1114cc51b518feba1ca58481a93202c585eb
viren_ahluwalia@yahoo.in <viren_ahluwalia@yahoo.in>
2021-12-15 21:38:31 -0800

    Updated the Project
    - Removed the AMG car model from the project.
    - Added new script for car wheel rotation.
    - Added the script to car prefabs

---
+++ SeniorProject_ML/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] private List<Transform> wheelTransform;
+    [SerializeField] private int rpm = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < wheelTransform.Count; ++i)
+            wheelTransform[i] = wheelTransform[i].GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < wheelTransform.Count; ++i)
+            wheelTransform[i].Rotate(rpm / 60.0f * 360.0f * Time.fixedDeltaTime, 0.0f, 0.0f);
+    }
+}
